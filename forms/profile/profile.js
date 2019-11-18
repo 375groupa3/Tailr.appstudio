f397050f398d07024fb4b4c690ebbf93619a1504
Lauren <laurenermer@hotmail.com>
2019-11-18 14:21:23 -0600

    1

--- forms/profile/profile.js
+++ forms/profile/profile.js
@@ -13,3 +13,14 @@ hmbSettings.onclick=function(){
     ChangeForm(logIn);
   }
 }
+
+profile.onshow=function(){
+var query = "SELECT * FROM `profile` WHERE user_id =" + '"' + user_id + '"'  
+  req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=lne99312&pass=Lnermer7&database=375groupa3&query=" + query);
+  if (req1.status == 200) { //transit worked.
+  results = JSON.parse(req1.responseText)
+    console.log(results) // this shows the array of arrays  
+    lblName.value = results[0][2]
+    imgProfilePic.src = results[0][1]
+    }
+}
