47e1187f624a38b408772a46996a222291b5ab62
Lauren <laurenermer@hotmail.com>
2019-11-18 14:28:59 -0600

    2

--- forms/profile/profile.js
+++ forms/profile/profile.js
@@ -15,12 +15,16 @@ hmbSettings.onclick=function(){
 }
 
 profile.onshow=function(){
-var query = "SELECT * FROM `profile` WHERE user_id =" + '"' + user_id + '"'  
-  req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=lne99312&pass=Lnermer7&database=375groupa3&query=" + query);
-  if (req1.status == 200) { //transit worked.
-  results = JSON.parse(req1.responseText)
-    console.log(results) // this shows the array of arrays  
-    lblName.value = results[0][2]
-    imgProfilePic.src = results[0][1]
+  var callQuery = "SELECT * FROM `profile` WHERE user_id =" + '"' + user_id + '"'
+  req3 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=jqb64765&pass=bia375&database=375groupa3&query=" + callQuery)
+  if (req3.status == 200) {
+    resultCall = JSON.parse(req3.responseText)
+    var callBlob = resultCall[0][1] + '.'
+    callBlob = callBlob.replace(/\s/gi, '+')
+    console.log(callBlob)
+    picArray = callBlob.split(".")
+    imgProfilePic.src = picArray[0]
     }
+  else
+    imgProfilePic.src = ''
 }
