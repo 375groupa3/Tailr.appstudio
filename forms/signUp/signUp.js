a4bfb8a768a694570c46d833cb8e2fd45573a122
alexsemler <alex.semler03@gmail.com>
2019-12-04 14:08:33 -0600

    Design

--- forms/signUp/signUp.js
+++ forms/signUp/signUp.js
@@ -12,12 +12,12 @@ btnSignUp1.onclick=function(){
     req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=lne99312&pass=Lnermer7&database=375groupa3&query=" + query);
     }
   else {
-    NSB.MsgBox("Sorry, your passwords don't match");
+    Msg.value = ("Sorry, your passwords don't match");
     }
     if (req1.status == 200) { //transit worked.
         if (req1.responseText == 500) {   // means the insert succeeded
             var result = JSON.parse(req1.responseText)
-            NSB.MsgBox("You have successfully signed up!")
+            Msg.value = ("You have successfully signed up!")
         } else
             NSB.MsgBox("There was a problem with signing you up.")
     } else {
